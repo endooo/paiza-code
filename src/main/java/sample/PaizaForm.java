d5f9d6d36dc409f6c67f7dc6c7ecafdb8d3fad3d
endooo <hiroshi.endooo@gmail.com>
2014-12-09 02:28:28 +0900

    commit sample code

---
+++ src/main/java/sample/PaizaForm.java
@@ -0,0 +1,19 @@
+package sample;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class PaizaForm {
+
+	public static void main (String[] args) {
+
+	    try(Scanner scanner = new Scanner(System.in)){
+            System.out.println(scanner.next());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+	}
+}
